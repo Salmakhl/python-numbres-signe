4444d36b82e4d8563f692eb8ae264f71e5eb3b4e
Salmakhl <147110378+Salmakhl@users.noreply.github.com>
2023-10-23 22:24:23 +0100

    Add files via upload

---
+++ python numbres signe.py
@@ -0,0 +1,6 @@
+a=float(input("entre the first numbre:"))
+b=float(input("entre the second numbre:"))
+if (a<0 and b<0) or (a>0 and b>0):
+    print("the numbers have the same signe") 
+else:
+    print("the numbers don't have the same signe.")
